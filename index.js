b4db09cd176f1d2f212d5cad9497dff9dc6055f8
quocvietktc <qvietktc@gmail.com>
2021-06-04 10:13:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
-const BootBot = require('./lib/BootBot');
+const BootBot = require('bootbot');
 
-module.exports = BootBot;
+const bot = new BootBot({
+  accessToken: 'EAACXQHTt31cBAEOLzBUEuQK3jAaKuP7ln3KtI9icnNTzZApLxSiK1RDmO6OnRyZAeQZAiR1Y7hZBLz0DyTTPqkZAH3iSjSOyAQ1nT793J0pg2gYD8LbGrJQVTzrFqi1F1p4Ny3PSmpbZBpTHukAjnrwX2IWr68TWZANmZAQSZAMEB5VN7QP5lPGb9',
+  verifyToken: 'chatbolt',
+  appSecret: '3d8584ead7aab807d4eef2a5c41efd9e'
+});
+
+bot.on('message', (payload, chat) => {
+  const text = 'alooo';
+  chat.say(`Echo: ${text}`);
+});
+
+bot.start();
